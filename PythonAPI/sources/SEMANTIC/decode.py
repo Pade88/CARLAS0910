7039307db669f465495578ba5121bfae71d91b4d
Pade88 <ovidiu.padeanu@gmail.com>
2021-06-11 21:59:28 +0300

    SW 1.07.1

--- PythonAPI/sources/SEMANTIC/decode.py
+++ PythonAPI/sources/SEMANTIC/decode.py
@@ -11,16 +11,20 @@ DATA_SET_SELECTOR = 1
 orange = [0, 165, 255]
 galben = [0, 255, 255]
 alb = [255, 255, 255]
+centru_sensor = (640, 700)
+
+# SF
+
 
 class ImageClassifier():
     def __init__(self):
         pass
 
-    #ToDo: polyfill intre benzi
+    # ToDo: polyfill intre benzi
     @staticmethod
     def detecteaza_marcaje_stradale(frame):
         lower_VAL = np.array([30, 220, 140], np.uint8)
-        upper_VAL = np.array([60, 240, 160], np.uint8)
+        upper_VAL = np.array([60, 255, 160], np.uint8)
         mask = cv2.inRange(frame, lower_VAL, upper_VAL)
 
         numar_marcaje = list()
@@ -33,15 +37,14 @@ class ImageClassifier():
             for coords in numar_marcaje:
                 cv2.rectangle(frame, (coords[0], coords[1]), (coords[0] + coords[2], coords[1] + coords[3]), orange, 2)
             cv2.putText(frame, "Trecere de pietoni", (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, alb, 3)
-            cv2.waitKey(500)
+            cv2.waitKey(100)
 
         cv2.imshow("Marcaje stradale", cv2.resize(mask, (990, 540)))
 
     @staticmethod
     def detecteaza_masini(frame):
         # BGR
-        cv2.line(frame, (300, 700), (700, 700), (0, 255, 0), thickness=3, lineType=8)
-        centru_sensor = (500, 700)
+        # cv2.line(frame, (300, 700), (980, 700), (0, 255, 0), thickness=3, lineType=8)
 
         lower_VAL = np.array([140, 0, 0], np.uint8)
         upper_VAL = np.array([160, 0, 0], np.uint8)
@@ -56,9 +59,10 @@ class ImageClassifier():
             min_x, max_x = min(x, min_x), max(x + w, max_x)
             min_y, max_y = min(y, min_y), max(y + h, max_y)
             centru_obiect = (int((2 * x + w) / 2), int((2 * y + h) / 2))
-            distanta = ImageClassifier.distanta(centru_obiect, centru_sensor) / 100
+            localizator_obiect = (int((x + w + x) / 2), y + h)
+            distanta = math.fabs((localizator_obiect[1] - centru_sensor[1]) / 100)
 
-            if w > 50 and h > 50:
+            if w > 80 and h > 80:
                 if distanta < 2:
                     cv2.rectangle(frame, (x, y), (x + w, y + h), orange, 2)
                 elif 2 < distanta < 4:
@@ -67,28 +71,29 @@ class ImageClassifier():
                     cv2.rectangle(frame, (x, y), (x + w, y + h), alb, 2)
 
                 cv2.circle(frame, centru_obiect, 1, (255, 255, 255), 5)
-                cv2.line(frame, centru_obiect, centru_sensor, (0, 255, 0), thickness=3, lineType=8)
+                cv2.line(frame, localizator_obiect, centru_sensor, (0, 255, 0), thickness=3, lineType=8)
                 cv2.putText(frame, "Vehicul {} metri".format(distanta), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, alb)
+
         # cv2.imshow("Vehicul", mask)
 
     # To be reworked
     @staticmethod
     def detecteaza_pieton(frame):
-        lower_VAL = np.array([40, 20, 180], np.uint8)
-        upper_VAL = np.array([60, 50, 220], np.uint8)
+        lower_VAL = np.array([40, 0, 200], np.uint8)
+        upper_VAL = np.array([60, 20, 255], np.uint8)
         blur = cv2.blur(frame, (5, 5))
         mask = cv2.inRange(blur, lower_VAL, upper_VAL)
-
         contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         for contour in contours:
             (x, y, w, h) = cv2.boundingRect(contour)
-            if w > 20 and h > 20:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), alb, 2)
-        # cv2.imshow("Pietoni", mask)
-
-    @staticmethod
-    def distanta(punct1, punct2):
-        return math.sqrt((punct2[0] - punct1[0]) ** 2 + (punct2[1] - punct2[1]) ** 2)
+            centru_obiect = (int((2 * x + w) / 2), int((2 * y + h) / 2))
+            localizator_obiect = (int((x + w + x) / 2), y + h)
+            distanta = math.fabs((localizator_obiect[1] - centru_sensor[1]) / 100)
+            if w > 30 and h > 30:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), alb, thickness=2, lineType=2)
+                cv2.putText(frame, "Pieton {} metri".format(distanta), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, alb)
+                cv2.line(frame, centru_obiect, centru_sensor, (255, 255, 0), thickness=1, lineType=1)
+        #cv2.imshow("Pietoni", mask)
 
     @staticmethod
     def decodeaza_R(frame):
@@ -140,20 +145,19 @@ cv2.setMouseCallback('Segmentata', mouseRGB)
 if __name__ == "__main__":
     cap_SS = cv2.VideoCapture(DATA_SET[DATA_SET_SELECTOR][1])
     cap_NM = cv2.VideoCapture(DATA_SET[DATA_SET_SELECTOR][0])
-
     while (cap_SS.isOpened()):
         ret, frame_SS = cap_SS.read()
         ret, frame_NM = cap_NM.read()
 
         if ret is False: break
 
-        #ToDo multiprocess
+        # ToDo multiprocess
         ImageClassifier.detecteaza_masini(frame_SS)
         ImageClassifier.detecteaza_marcaje_stradale(frame_SS)
-        ImageClassifier.detecteaza_pieton(frame_SS)
-        ImageClassifier.decodeaza_R(frame_SS)
-        ImageClassifier.HAAR_Classifier(frame_NM)
+        #ImageClassifier.detecteaza_pieton(frame_SS)
+        #ImageClassifier.decodeaza_R(frame_SS)
+        #ImageClassifier.HAAR_Classifier(frame_NM)
 
         cv2.imshow("Segmentata", cv2.resize(frame_SS, (990, 540)))
 
-        cv2.waitKey(1)
+        cv2.waitKey(16)
